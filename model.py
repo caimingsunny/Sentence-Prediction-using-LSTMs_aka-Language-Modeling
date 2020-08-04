f771fea50192eede2e4aea88b92e1676cb0fdc18
caimingsunny <67154189+caimingsunny@users.noreply.github.com>
2020-08-04 12:20:14 +0800

    Update model.py

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ def read_file(filepath):
 		str_text = f.read()
 	return str_text
 
-text = read_file('EU-AU-Description-19-9-2019_qtr1.txt')
+text = read_file('generating.txt')
 tokens = text.split(" ")
 tokens.pop(0)
 
